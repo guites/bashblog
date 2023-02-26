3e8ba1ff26293b21cb117f68a6fb35089f72c446
Guilherme Garcia <gui.garcia67@gmail.com>
2023-02-26 12:52:24 -0300

    Remove google analytics related features

--- bb.sh
+++ bb.sh
@@ -36,13 +36,6 @@ global_variables() {
     # CC by-nc-nd is a good starting point, you can change this to "&copy;" for Copyright
     global_license="CC by-nc-nd"
 
-    # If you have a Google Analytics ID (UA-XXXXX) and wish to use the standard
-    # embedding code, put it on global_analytics
-    # If you have custom analytics code (i.e. non-google) or want to use the Universal
-    # code, leave global_analytics empty and specify a global_analytics_file
-    global_analytics=""
-    global_analytics_file=""
-
     # Leave this empty (i.e. "") if you don't want to use feedburner, 
     # or change it to your own URL
     global_feedburner=""
@@ -183,29 +176,6 @@ markdown() {
 }
 
 
-# Prints the required google analytics code
-google_analytics() {
-    [[ -z $global_analytics && -z $global_analytics_file ]]  && return
-
-    if [[ -z $global_analytics_file ]]; then
-        echo "<script type=\"text/javascript\">
-
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', '${global_analytics}']);
-        _gaq.push(['_trackPageview']);
-
-        (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-
-        </script>"
-    else
-        cat "$global_analytics_file"
-    fi
-}
-
 # Reads HTML file from stdin, prints its content to stdout
 # $1    where to start ("text" or "entry")
 # $2    where to stop ("text" or "entry")
@@ -293,13 +263,13 @@ edit() {
 #
 # $1 the file
 #
-# Return 0 (bash return value 'true') if the input file is a component (footer, header, analytics)
+# Return 0 (bash return value 'true') if the input file is a component (footer, header)
 # or 1 (bash return value 'false') if it is a complete page
 is_included_in_sitemaps() {
     name=${1#./}
 
     case $name in
-    ( "$footer_file" | "$header_file" | "$global_analytics_file" )
+    ( "$footer_file" | "$header_file" )
         return 0 ;;
     esac
 
@@ -322,7 +292,7 @@ is_boilerplate_file() {
     done
 
     case $name in
-    ( "$index_file" | "$archive_index" | "$tags_index" | "$footer_file" | "$header_file" | "$global_analytics_file" | "$prefix_tags"* )
+    ( "$index_file" | "$archive_index" | "$tags_index" | "$footer_file" | "$header_file" | "$prefix_tags"* )
         return 0 ;;
     ( * ) # Check for excluded
         for excl in "${html_exclude[@]}"; do
@@ -360,7 +330,6 @@ create_html_page() {
         if [[ $index == yes ]] && [ -n "$index_description_meta" ]; then
             echo "<meta name='description' content='$index_description_meta'>"
         fi
-        google_analytics
         echo "</head><body>"
         # stuff to add before the actual body content
         [[ -n $body_begin_file ]] && cat "$body_begin_file"
